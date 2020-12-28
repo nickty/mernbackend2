8df4af3b4bbd7c2cfbf3ea38f28ea3050e54a635
mizan <nickty.86@gmail.com>
2020-12-28 20:52:44 +0600

    inital commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+
+//import User from './models/User.js'
+import User from './models/User.js'
+
+const app = express()
+
+
+//db connection 
+const mongoUrl = "mongodb://localhost:tryagain"
+
+mongoose.connect(mongoUrl, {
+    useCreateIndex: true, 
+    useNewUrlParser: true, 
+    useFindAndModify: false
+}).then(()=>console.log('Db connected'))
+
+
+app.use(bodyParser.json())
+
+app.post('/register', async (req, res) => {
+
+    const newUser = new User(req.body)
+
+    await newUser.save()
+
+    res.send(newUser)
+    
+
+})
+
+
+app.listen(5000, ()=>console.log('Server is ON'))
